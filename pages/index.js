aae5491631e08fb4694b22c6c74f41847b5bffc5
unknown <emailkamu@gmail.com>
2025-07-29 21:51:24 +0800

    Update halaman home

--- pages/index.js
+++ pages/index.js
@@ -5,9 +5,7 @@ export default function Home() {
     <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100 text-center">
       <h1 className="text-3xl font-bold mb-4">Selamat datang di Wedding App!</h1>
       <p className="mb-6">Silakan login untuk mengakses dashboard Anda</p>
-      <Link href="/login" className="text-blue-600 underline">
-        Go to Login Page
-      </Link>
+      <Link href="/login" className="text-blue-600 underline">Go to Login Page</Link>
     </div>
   );
 }
